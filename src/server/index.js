38dfe63aa3238163c415d406110ef340ca161982
Jiwon <jiwon1.han@gmail.com>
2021-03-09 22:51:35 -0800

    testing deploymenet

--- src/server/index.js
+++ src/server/index.js
@@ -21,6 +21,10 @@ app.use(express.static('dist'))
 app.use(express.static(path.join(__dirname, 'dist')));
 app.set('port', process.env.PORT || 8080);
 
+const server = app.listen(app.get('port'), function() {
+  console.log('listening on port ', server.address().port);
+});
+
 const application_key = process.env.API_KEY;
 
 
