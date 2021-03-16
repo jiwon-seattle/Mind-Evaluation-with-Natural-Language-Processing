064d5ed46adf7e4d7f301f21db26bb8e0a6e42c9
Jiwon <jiwon1.han@gmail.com>
2021-03-15 17:34:31 -0700

    fixed issues

--- src/client/js/updateUI.js
+++ src/client/js/updateUI.js
@@ -2,15 +2,14 @@ import { polarityChecker } from '../js/formHandler'
 
 function updateUI(element, content) {
   if (content.confidence == undefined) {
-    element.innerHTML = `<div class="results">Sorry, I can't analyze your feelings</div>`;
+    element.innerHTML = `<div class="output">Sorry, I can't analyze your feelings</div>`;
   }
 
   else {
-
-    element.innerHTML = `<div class="results"><div>Polarity: POSITIVE <br></div>
-    <div>Confidence: 100 <br></div>
-    <div>Subjectivity: SUBJECTIVE <br></div>
-    <div>Irony: NONIRONIC<div></div>`;
+    element.innerHTML = `<div class="output"><div>Polarity: ${polarityChecker(content.score_tag)} <br></div>
+    <div>Confidence: ${content.confidence} <br></div>
+    <div>Subjectivity: ${content.subjectivity} <br></div>
+    <div>Irony: ${content.irony}</div></div>`;
   }
 }
 
